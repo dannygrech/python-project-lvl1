15bad4c5b6f2cc12134cc2c9e0944d690a1b8ba4
dannygrech <gorodovaj@gmail.com>
2022-07-13 18:53:04 +0300

    мелкие изменения по ревью

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,24 +1,25 @@
 import prompt
 
-from brain_games.scripts import brain_games
 
 ROUNDS_COUNT = 3
 
 
 def run(games):
+    print('Welcome to the Brain Games!!!!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
     count = ROUNDS_COUNT
-    name = brain_games.main()
     print(games.DESCRIPTION)
     while count:
         question, correct_answer = games.generate_data()
         print(f'Question: {question}')
-        right_answer = prompt.string('Your answer: ')
-        if right_answer == correct_answer:
+        answer = prompt.string('Your answer: ')
+        if answer == correct_answer:
             count -= 1
             print('Correct!')
         else:
-            print(f'"{right_answer}" is the wrong answer.'
+            print(f'"{answer}" is the wrong answer.'
                   f' Correct answer is "{correct_answer}"')
             print(f'Try again,{name}!')
             break
-    print(f"Congratulations, {name}!")
+        print(f"Congratulations, {name}!")
